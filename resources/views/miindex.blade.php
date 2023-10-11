2bf36f3fb8c1d7525edd36ed72c9624349ac7242
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2023-10-11 10:08:57 -0600

    Modificación de las cartas con imagenes y del margen

--- resources/views/miindex.blade.php
+++ resources/views/miindex.blade.php
@@ -7,19 +7,18 @@
       </div>
       <div class="col-sm-12 col-md-8 col-lg-8 col-xl-8">
         <h2 class="">Votación Electrónica y Recomendación de noticias</h2>
-
       </div>
     </div>
+    <hr />
   </div>
-  <hr />
   <!-- End Title Section -->
 
-  <!-- Card section -->
-  <section class="section">
-    <div class="row justify-content-center align-items-center">
+  <!-- Card Section -->
+  <section class="row" style="background-color: rgb(235, 231, 231);">
+      <!-- Voting -->
       <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
         <div class="card">
-          <img src="{{ asset('NiceAdmin/assets/img/card.jpg') }}" class="card-img-top" alt="...">
+          <img src="{{ asset('NiceAdmin/assets/img/electronic voting.jpg') }}" class="card-img-top" alt="voting image">
           <div class="card-body">
             <h5 class="card-title">Votación electrónica</h5>
             <p class="card-text">Vota de manera <b><i>electrónica, fácil y segura</i></b> por los candidatos a la presidencia de México</p>
@@ -27,19 +26,10 @@
         </div>
       </div>
 
+      <!-- Forum -->
       <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
         <div class="card">
-          <img src="{{ asset('NiceAdmin/assets/img/card.jpg') }}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Recomendación de noticias</h5>
-            <p class="card-text">Infórmate de las noticias del momento con nuestro <i>sistema de recomendación de noticias</i> basado en tus preferencias.</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
-        <div class="card">
-          <img src="{{ asset('NiceAdmin/assets/img/card.jpg') }}" class="card-img-top" alt="...">
+          <img src="{{ asset('NiceAdmin/assets/img/forum.jpg') }}" class="card-img-top" alt="forum image">
           <div class="card-body">
             <h5 class="card-title">Foro de propuestas</h5>
             <p class="card-text"><b>Comparte</b> tus propias propuestas de ley en un foro al resto de ciudadanos y vota por las que más te gusten.</p>
@@ -47,20 +37,31 @@
         </div>
       </div>
 
+      <!-- News -->
       <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
         <div class="card">
-          <img src="{{ asset('NiceAdmin/assets/img/card.jpg') }}" class="card-img-top" alt="...">
+          <img src="{{ asset('NiceAdmin/assets/img/news.jpg') }}" class="card-img-top" alt="news image">
+          <div class="card-body">
+            <h5 class="card-title">Recomendación de noticias</h5>
+            <p class="card-text">Infórmate de las noticias del momento con nuestro <i>sistema de recomendación de noticias</i> basado en tus preferencias.</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- Validation -->
+      <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
+        <div class="card">
+          <img src="{{ asset('NiceAdmin/assets/img/validation.jpg') }}" class="card-img-top" alt="validation image">
           <div class="card-body">
             <h5 class="card-title">Valida tu cuenta utilizando tu INE</h5>
             <p class="card-text">Valida tu cuenta de forma <b>segura</b> y <b>fácil</b> por medio de tu identificación oficial INE o IFE.</p>
           </div>
         </div>
       </div>
-
-    </div>
   </section>
   <!-- End Card Section -->
 
+  <!-- Graphic Section -->
   <section class="row justify-content-center align-items-center">
     <div class="col-sm-12 col-md-10 col-lg-8 col-lg-6">
       <div class="card">
@@ -79,17 +80,15 @@
                     'PAN',
                     'PRI',
                     'Independientes',
-                    'Candidatos no registrados',
                     'Votos Nulos'
                   ],
                   datasets: [{
-                    data: [53.20, 22.27, 16.41, 5.23, 0.06, 2.83],
+                    data: [53.19, 22.27, 16.40, 5.23, 2.91],
                     backgroundColor: [
                       'rgb(213, 36, 33)',
                       'rgb(52, 166, 254)',
                       'rgb(42, 162, 60)',
                       'rgb(175, 60, 198)',
-                      'rgb(83, 54, 69)',
                       'rgb(171, 171, 171)'
                     ],
                     hoverOffset: 6
@@ -104,6 +103,7 @@
       </div>
     </div>
   </section>
+  <!-- End Graphic Section -->
 
 
 </x-template-nice-admin>
